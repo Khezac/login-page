4c89f00d016ac10454dd0c8205b50ad505640033
Khezac Khalleb <152409554+Khezac@users.noreply.github.com>
2024-02-28 15:41:30 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html","./login.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
